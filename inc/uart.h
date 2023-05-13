bcb4d33c9688cb6b196f0bb095b58cde83f3c58a
Mahmoud Abuelnaga <Abuelnaga.m0@gmail.com>
2023-05-13 22:00:37 +0300

    UART5 functions

--- inc/uart.h
+++ inc/uart.h
@@ -12,5 +12,10 @@ void uart2_send_string(char * string);
 char uart2_receive(void);
 void uart2_receive_string(char * string, uint32_t len); // Not complete
 
-
+// UART5
+void uart5_init(void);
+void uart5_send(uint8_t character);
+void uart5_send_string(char * string);
+char uart5_receive(void);
+void uart5_receive_string(char * string, uint32_t len);
 #endif
--- src/uart.c
+++ src/uart.c
@@ -68,3 +68,81 @@ void uart2_receive_string(char * string, uint32_t len) {    // Not complete
         string[i] = received;
     }
 }
+
+/*
+A function that sets up port E. It enables UART5 for port E.
+Enable UART For port E steps:
+1. Enable clock for UART5 in port E
+2. Enable clock for port E GPIO pins
+3. Disable UART5
+4. Configure its UART5 registers
+5. Configure port E GPIO pins' registers
+*/
+void uart5_init() {
+    SYSCTL_RCGCUART_R |= SYSCTL_RCGCUART_R5; // Enable clock for UART5
+    SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R4; // Enable clock for port E GPIO pins
+    while((SYSCTL_PRGPIO_R & SYSCTL_PRGPIO_R4) == 0); // Wait until port E clock is connected
+
+    // Enable commiting changes
+    GPIO_PORTE_LOCK_R = GPIO_LOCK_KEY; // Unlock port E for changes
+    GPIO_PORTE_CR_R = 0x3F; // Allow commiting changes to all pins of port E
+
+    // Configure UART5
+    UART5_CTL_R &= ~UART_CTL_UARTEN;    // Disable the UART
+    UART5_IBRD_R = 104; // Setup the BaudRate to be 9600
+    UART5_FBRD_R = 11;  // Setup the BaudRate to be 9600
+    UART5_CC_R |= UART_CC_CS_SYSCLK; // Connect system clock to UART5
+    UART5_LCRH_R = (UART_LCRH_WLEN_8 | UART_LCRH_FEN); // Word length 8, Enable FIFO, No parity
+    UART5_CTL_R = (UART_CTL_UARTEN | UART_CTL_RXE | UART_CTL_TXE);  // Enable UART5, RX, TX
+
+    // Configure GPIO pins of port E (PE5 -> Tx and PE4 -> Rx)
+    GPIO_PORTE_DEN_R = 0x3F;    // Digial enable all pins
+    GPIO_PORTE_AMSEL_R = 0; // Disable analog mode
+    GPIO_PORTE_AFSEL_R = 0x30;  // Enable alternative funcion for PE4 and PE5
+    GPIO_PORTE_PCTL_R = (GPIO_PORTE_PCTL_R & 0x00FFFF) | (GPIO_PCTL_PE5_U5TX | GPIO_PCTL_PE4_U5RX); // PE4 and PE5 act as UART pins
+    // GPIO_PORTE_PCTL_R |= (GPIO_PCTL_PE5_U5TX | GPIO_PCTL_PE4_U5RX); // PE4 and PE5 act as UART pins
+
+}
+
+/*
+A function that send character through UART5.
+*/
+void uart5_send(uint8_t character) {
+    while((UART5_FR_R & UART_FR_TXFF) != 0);    // wait until the transmitter buffer isn't full
+    UART5_DR_R = character;
+}
+
+/*
+Sends string over UART5.
+Takes:
+char * string: pointer to string (char array) to send over UART.
+*/
+void uart5_send_string(char * string) {
+    while(*string) {
+        uart5_send(*string);
+        string++;
+    }
+}
+
+/*
+A function that receives character from UART5
+*/
+char uart5_receive() {
+    while ((UART5_FR_R & UART_FR_RXFE) != 0);   // wait till you recieve data
+    return (unsigned char)(UART5_DR_R & 0xFF);
+}
+
+/*
+A function that receives string over UART5.
+Takes:
+1. char * string: pointer to character array, where to store the received characters
+2. uint32_t len: length of array where to store characters
+*/
+void uart5_receive_string(char * string, uint32_t len) {    // Not complete
+    char received;
+    int i;
+    for(i = 0; i < len; i++) {
+        received = uart5_receive();
+        string[i] = received;
+    }
+}
